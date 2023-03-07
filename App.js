68ba2985b7f7e742c77f6ad6d7baa63fe3001e38
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-07 17:03:08 +0200

    started_text

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TeslaClone!</Text>
+      
+      <View style={styles.carContainer}> 
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+    </View>
+
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  carContainer: {
+    width: "100%",
+    height: "100%",
+  },
+  titles: {
+    marginTop: "30%",
+    width: "100%",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "600",
+  },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
   },
 });
