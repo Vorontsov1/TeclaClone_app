417047d2981d7aac69ba9d63d8f3234aba8e005e
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-07 21:40:50 +0200

    finish_button

--- components/StyledButton/index.js
+++ components/StyledButton/index.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import { View, Text, Pressable } from 'react-native';
-import styles from './styles';
-
+import React from "react";
+import { View, Text, Pressable } from "react-native";
+import styles from "./styles";
 
 const StyledButton = (props) => {
+  const { type, content, onPress } = props;
 
-    const type = props.type;
-  
-    const backgroundColor = type === 'primary' ? '#171A20CC' : '#ffffffa6';
-    const textColor = type === "primary" ? "#FFFFFF" : "#171A20";
-
+  const backgroundColor = type === "primary" ? "#171A20CC" : "#ffffffa6";
+  const textColor = type === "primary" ? "#FFFFFF" : "#171A20";
 
   return (
     <View style={styles.container}>
       <Pressable
         style={[styles.button, { backgroundColor: backgroundColor }]}
-        onPress={() => {
-          console.warn("hey man");
-        }}
+        onPress={() => onPress()}
       >
-        <Text style={[styles.text, {color: textColor}]}>Custom Order</Text>
+        <Text style={[styles.text, { color: textColor }]}>{content}</Text>
       </Pressable>
     </View>
   );
 };
 
 export default StyledButton;
-
-
