a14d0eff8aaabea26236bed7ddc22337b37c7046
Vorontsov1 <evgeniyvorontsov1@gmail.com>
2023-03-08 10:37:26 +0200

    added_CarItem

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import CarItem from './components/CarItem';
+import { StyleSheet, View } from "react-native";
+import CarsList from "./components/CarsList";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CarItem
-        name={'Model 3'}
-        tagline={'Order Online For'}
-        taglineCTA={'Touchless delivery'}
-        image={require('./assets/images/Model3.jpeg')}
-      />
-
+      <CarsList />
       <StatusBar style="auto" />
     </View>
   );
@@ -26,4 +20,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
- 
